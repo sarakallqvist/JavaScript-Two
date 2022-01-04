e11708ea47160a84f5210d78846438f4bf3bbb07
Sara Källqvist <sarakallqvist@gmail.com>
2022-01-04 20:52:05 +0100

    Final commit

--- script.js
+++ script.js
@@ -1,41 +1,69 @@
 // BANK ACCOUNT
 
-// Start by creating the object and the properties. You can leave the functions empty for now if you want.
-// You'll find all the information about what properties the object should have in the readme file.
-
 const account = {
 accountName: "Sara Källqvist",
 balance: 100,
-getBalance: function () { },
-deposit: function () { },
-withdrawal: function () { },
-getAccountName: function () { },
-accountError: function () { },
-exitAccount: function () { },
-// In the deposit and withdrawal function use this code to show the prompt
-parseFloat(prompt("Put some proper message here"));
-// You need to change the message of course. You should also do some research why parseFloat() is used here
-// answer by making a comment in the code.
-// Be aware of that you need to add some code to this so you can display the actual input.
-
-// In the deposit and withdrawal function you also need to handle som potential errors.
-// To handle one of the potential error you can use this piece of code
-isNaN(variableName);
-// you need to change the variableName to your actual variable.
-// you also need to check some other conditions here and use a logical operator.
-// try to say the sentece out loud or think it:
-// "A user should not be able to put 0 or negative numbers or empty string or a type that is not a number"
-// Do you know what operator you should use?
-
-// In the accountError and accountExit function you can use this to show a message to the user:
-alert("your code here and maybe properties");
-// of course you need to put a proper message and you might need to display some of the properties.
-
-// STARTER FUNCTION
-function atm() {
-  let choice = parseInt(prompt("Display menu choices here"));
-  //prompt user for choice.
-
-  // you can use the variable choice for your switch or if/else statement
-  // so choice will hold the value of the user input.
+getBalance: function () {
+  alert ("Your balance is: " + this.balance)
+ },
+deposit: function () {
+  let choice = parseFloat(prompt("How much would you like to deposit?"))
+  if (isNaN(choice) )
+  {
+    this.accountError ("Error, please choose a numeric value.")
+    return
+  }
+  this.balance = this.balance + choice
+  alert ("Thank you for your deposit. Your new balance is: " +this.balance)
+},
+withdrawal: function () {
+  let choice = parseFloat(prompt("How much would you like to withdraw?"))
+  if (isNaN(choice) )
+  {
+    this.accountError ("Error, please choose a numeric value.")
+    return
+  }
+  if (choice >this.balance )
+  {
+    this.accountError ("Amount exceeds account balance.")
+    return
+  }
+  this.balance = this.balance - choice
+  alert ("Thank you for your withdrawal. Your new balance is: " +this.balance)
+},
+getAccountName: function () {
+  alert ("Your account name is: " +this.accountName)
+},
+accountError: function (message) {
+  alert (message)
+},
+exitAccount: function () {
+  alert ("Thank you for your visit, have a nice day!")
+window.close ()
+},
 }
+// parseFloat konverterar nummer, som för datorn är samma som text, till ett numerärt värde. Möjliggör att addera, subtrahera osv.
+
+function atm() {
+  let choice = parseInt(prompt
+    ("Select a choice: 1. See Balance 2. Make a deposit 3. Make a Withdrawal 4. Get Account Name 5. Exit"));
+  if (choice === 1) {
+    account.getBalance ()
+  }
+  else if (choice === 2) {
+account.deposit ()
+  }
+  else if (choice === 3) {
+account.withdrawal ()
+  }
+  else if (choice === 4) {
+account.getAccountName ()
+  }
+  else if (choice === 5) {
+account.exitAccount ()
+  }
+
+  atm ()
+}
+
+atm()
